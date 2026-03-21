f5a8ab403e8c76e41339127617e8bfc81452cce9
Naveen <baddigamnaveenkumarreddy1910@gmail.com>
2026-03-21 17:52:26 +0530

    feat: removed comments in next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 // import jwt from "jsonwebtoken";
 import { NextApiRequest, NextApiResponse } from "next";
-import NextAuth, { NextAuthOptions, Session } from "next-auth";
-import { JWT } from "next-auth/jwt";
-import CognitoProvider from "next-auth/providers/cognito";
+import NextAuth, {
+  NextAuthOptions,
+  // Session
+} from "next-auth";
+// import { JWT } from "next-auth/jwt";
+// import CognitoProvider from "next-auth/providers/cognito";
 // import { serialize } from "cookie";
 import CredentialsProvider from "next-auth/providers/credentials";
 import crypto from "crypto";
@@ -10,12 +13,12 @@ import {
   CognitoIdentityProviderClient,
   InitiateAuthCommand,
 } from "@aws-sdk/client-cognito-identity-provider";
-import { AdapterUser } from "next-auth/adapters";
+// import { AdapterUser } from "next-auth/adapters";
 
-interface ICustomSession extends Session {
-  accessToken: string;
-  refreshToken: string;
-}
+// interface ICustomSession extends Session {
+//   accessToken: string;
+//   refreshToken: string;
+// }
 
 function calculateSecretHash(username: string) {
   return crypto
@@ -76,7 +79,7 @@ export const nextAuthOptions: NextAuthOptions = {
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.accessToken = (user as AdapterUser).accessToken;
+        token.accessToken = user.accessToken;
         token.idToken = user.idToken;
         token.refreshToken = user.refreshToken;
       }
@@ -90,64 +93,6 @@ export const nextAuthOptions: NextAuthOptions = {
     },
   },
 };
-// const nextAuthOptions = (res: NextApiResponse) => {
-//   return {
-//     providers: [
-//       CognitoProvider({
-//         clientId: process.env.COGNITO_CLIENT_ID as string,
-//         clientSecret: process.env.COGNITO_CLIENT_SECRET as string,
-//         issuer: process.env.COGNITO_ISSUER,
-//       }),
-//     ],
-//     callbacks: {
-//       async jwt({ token, user, account }) {
-//         console.log(`Inside NextAuth jwt callback.`);
-//         if (user) {
-//           token.accessToken = account?.access_token;
-//           token.refreshToken = account?.refresh_token;
-//           token.idToken = account?.id_token;
-//         }
-//         // if (account?.id_token) {
-//         //   console.log(`Setting id_token as Cookie from NextAuth jwt callback.`);
-//         //   res.setHeader(
-//         //     "Set-Cookie",
-//         //     serialize("ID_TOKEN_COOKIE_KEY", account?.id_token || "", {
-//         //       path: "/",
-//         //     }),
-//         //   );
-//         //   const decoded = jwt.decode(account.id_token);
-//         //   if (decoded && typeof decoded === "object") {
-//         //     if ("cognito:groups" in decoded) {
-//         //       const userRole = decoded["cognito:groups"][0];
-//         //       token.userRole = userRole;
-//         //     }
-//         //   }
-//         // }
-//         return token;
-//       },
-
-//       async session({
-//         session,
-//         token,
-//       }: {
-//         session: Session;
-//         token: JWT;
-//       }): Promise<ICustomSession> {
-//         console.log(`Inside NextAuth session callback.`);
-//         if (token && session.user) {
-//           console.log(
-//             `Attaching tokens to the session object in NextAuth session callback.`,
-//           );
-//           session.accessToken = token.accessToken as string;
-//           session.refreshToken = token.refreshToken as string;
-//           session.idToken = token.idToken as string;
-//         }
-//         return session as ICustomSession;
-//       },
-//     },
-//     secret: process.env.NEXTAUTH_SECRET,
-//   } as NextAuthOptions;
-// };
 
 const Handler = (req: NextApiRequest, res: NextApiResponse) => {
   try {
